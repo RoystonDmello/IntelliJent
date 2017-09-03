4472d9d476075c73f4370287d72f965e9f6ce914
RoystonDmello <dmelloroyston42@gmail.com>
2017-09-04 02:44:52 +0530

    Spotify app added to init

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,15 @@
+import os
+
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import spotipy as sp
+import spotipy.util as util
+
+
+from config import credentials
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,6 +17,16 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+username, scope, client_id, client_secret, redirect_uri = credentials
 
+cache_filename = ".cache-{0}".format(username)
+if os.path.isfile(cache_filename):
+    os.remove(cache_filename)
+
+token = util.prompt_for_user_token(username, scope,
+                                       client_id, client_secret,
+                                       redirect_uri)
+
+spotify = sp.Spotify(auth=token)
 
 from backend import views, models
--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,4 @@
 
 from backend import app
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(debug=True)
